20362fb7dcf34fe13e07e16103d2c29b919b253f
rlevilla42 <rlevilla42@gmail.com>
2024-10-20 22:55:57 +0200

    ?

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-touch init.sql
-chmod 777 init.sql
-
-echo "CREATE DATABASE IF NOT EXISTS wordpress;" >> init.sql
-echo "FLUSH PRIVILEGES;" >> init.sql
-echo "GRANT ALL ON *.* TO 'buakaw'@'localhost' IDENTIFIED BY 'buakaw' WITH GRANT OPTION;" >> init.sql
-echo "FLUSH PRIVILEGES;" >> init.sql
-echo "CREATE USER IF NOT EXISTS 'buakaw'@'%' IDENTIFIED BY 'buakaw';" >> init.sql
-echo "GRANT ALL ON wordpress.* TO 'buakaw'@'%' IDENTIFIED BY 'buakaw';"  >> init.sql
-echo "FLUSH PRIVILEGES;" >> init.sql
-
-mysql -h localhost < init.sql
-
-#exec mysqld
+touch init.
--- srcs/requirements/wordpress/wp-config.sh
+++ srcs/requirements/wordpress/wp-config.sh
@@ -9,4 +9,4 @@ chmod +x wp-cli.phar
 ./wp-cli.phar config create --dbname=db_wordpress --dbuser=buakaw --dbpass=buakaw --dbhost=mariadb --allow-root --force
 ./wp-cli.phar core install --url=rlevilla.42.fr --title=wptitle --admin_user=buakaw --admin_password=buakaw --admin_email=caca@gmail.com --allow-root
 
-php-fpm7.4 -F
+php-fpm7.4 -
